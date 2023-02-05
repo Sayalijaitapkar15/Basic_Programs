df15670ea101d9622d4c74fdf4f47805ca537bb9
Sayalijaitapkar15 <123749060+Sayalijaitapkar15@users.noreply.github.com>
2023-02-05 18:00:52 +0530

    Upload my basic C++ problems.

---
+++ Problem1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int x , y ;
+    cout<<"Enter a value of x : ";
+    cin>>x;
+    cout<<"Enter a value of y : ";
+    cin>>y;
+    cout<<x*y;
+    return 0 ;
+}
---
+++ Problem3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int length , breadth ;
+    cout<<"Enter length of a rectangle : ";
+    cin>>length;
+    cout<<"Enter breadth of a rectangle : ";
+    cin>>breadth;
+    cout<<length * breadth <<endl;
+    return 0 ;
+}
